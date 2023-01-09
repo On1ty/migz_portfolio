aba7e13f369d00908950c4f13190db0c475a3308
MigzSakalam <juanmiguel.pallo@paybps.com>
2023-01-09 17:44:16 +0800

    comment code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+// import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import { setupCounter } from './counter.js'
 
 // document.querySelector('#app').innerHTML = `
 //   <div>
